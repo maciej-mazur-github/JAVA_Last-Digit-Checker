89948940393e310ee671a74bfd1a140c84b17e23
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-03-28 23:02:39 +0200

    Add isValid and main method

--- src/LastDigitChecker.java
+++ src/LastDigitChecker.java
@@ -1,6 +1,6 @@
 public class LastDigitChecker {
     public static boolean hasSameLastDigit(int arg1, int arg2, int arg3) {
-        if(arg1 < 10 || arg1 > 1000 || arg2 < 10 || arg2 > 1000 || arg3 < 10 || arg3 > 1000) {
+        if(!(isValid(arg1) && isValid(arg2) && isValid(arg3))) {
             return false;
         }
 
@@ -9,5 +9,17 @@ public class LastDigitChecker {
         int lastDigit3 = arg3 % 10;
 
         return lastDigit1 == lastDigit2 || lastDigit2 == lastDigit3 || lastDigit1 == lastDigit3;
+    }
+
+    public static boolean isValid(int number) {
+        return number >= 10 && number <= 1000;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasSameLastDigit(978, 48, 64));
+        System.out.println(hasSameLastDigit(9, 109, 909));
+        System.out.println(hasSameLastDigit(77, -1, 27));
+        System.out.println(hasSameLastDigit(11, 232, 971));
+        System.out.println(hasSameLastDigit(972, 48, 62));
     }
 }
